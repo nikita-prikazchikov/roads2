a16295888813a822882a607f31a899f97d949349
Nikita Prikazchikov <nikita.prikazchikov@gmail.com>
2016-03-13 12:57:22 +0300

    Updated source data

--- app/js/models.js
+++ app/js/models.js
@@ -5,6 +5,7 @@ function ElementStress(stress){
     this.S = stress.S;
     this.A = 0;
     this.work = [];
+    this.k = 108500 * 108500 * 220 / (3 * 200000 * 9667500);
 }
 
 ElementStress.prototype.calculate = function() {
@@ -17,15 +18,15 @@ ElementStress.prototype.calculate = function() {
         var ai = 0;
         if (s1 > 0 && s2 > 0 && s2 > s1) {
             ai += this.getDeltaA(s2 * s2 - s1 * s1);
-            console.log("positive: s[i]: " + s1 + " s[i+1]: " + s2 + " work: " + ai);
+//            console.log("positive: s[i]: " + s1 + " s[i+1]: " + s2 + " work: " + ai);
         }
         else if (s1 < 0 && s2 < 0 && s1 > s2) {
             ai += this.getDeltaA(s2 * s2 - s1 * s1);
-            console.log("negative: s[i]: " + s1 + " s[i+1]: " + s2 + " work: " + ai);
+//            console.log("negative: s[i]: " + s1 + " s[i+1]: " + s2 + " work: " + ai);
         }
         else if ((s1 >= 0 && s2 < 0) || (s1 <= 0 && s2 > 0)) {
             ai += this.getDeltaA(s2 * s2);
-            console.log("near zero: s[i]: " + s1 + " s[i+1]: " + s2 + " work: " + ai);
+//            console.log("near zero: s[i]: " + s1 + " s[i+1]: " + s2 + " work: " + ai);
         }
         else {
             console.log("not included: s[i]: " + s1 + " s[i+1]: " + s2 + " work: " + ai);
@@ -37,5 +38,5 @@ ElementStress.prototype.calculate = function() {
 
 ElementStress.prototype.getDeltaA = function(deltaSigma) {
     //return deltaSigma * this.S * this.l / this.E;
-    return deltaSigma * deltaSigma * 108500 * 108500 * 220 / (3 * 200000 * 9667500);
+    return deltaSigma * this.k;
 };
